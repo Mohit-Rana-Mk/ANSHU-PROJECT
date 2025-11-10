77d2a9bf15e236687fd733645a6ff6be798a7281
Mohit-Rana-MK <mkrana.280504@gmail.com>
2025-11-11 00:03:20 +0530

    Initial commit for Health GPS IoT Tracker

--- index.js
+++ index.js
@@ -11,9 +11,16 @@ const io = new Server(server, {
   }
 });
 
+const path = require('path');
+
 app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
+
+// Serve the main page
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 // In-memory store for last N data points per device
 const MAX_POINTS = 200;
