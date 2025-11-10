e2a8f21ba915fbfb453b123cbf45c3a07a003dc3
Mohit-Rana-MK <mkrana.280504@gmail.com>
2025-11-11 00:35:04 +0530

    Update server to serve index.html from root

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(express.static('public'));
 
 // Serve the main page
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 // In-memory store for last N data points per device
